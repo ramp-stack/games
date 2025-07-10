c89a20f311d127ca9d5ee52aa5a7a39dfe74a584
gus-eisen <guseisenberch@proton.me>
2025-07-10 16:43:31 -0500

    Implemented conditional compilation for game selection.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    #[cfg(not(target_arch="wasm32"))]
-    {
-        main::maverick_main()
-    }
+    #[cfg(all(not(target_arch = "wasm32"), feature = "galaga"))]
+    main::maverick_main();
+
+    #[cfg(feature = "default")]
+    println!("Enter valid game name with `cargo run --features game_name. Use lower case.`");
 }
 
